6ade6065a8cb97ee41333c9bd975f6f722186870
KobeLenjou <kobe@lenjou.be>
2025-01-03 15:58:56 +0100

    Update

--- src/Core/Resolvers/SqlResponseHelpers.cs
+++ src/Core/Resolvers/SqlResponseHelpers.cs
@@ -121,7 +121,7 @@ namespace Azure.DataApiBuilder.Core.Resolvers
                                   after);
 
             //Get the element RecordCount from the first element of the array
-            JsonElement recordCountElement = rootEnumerated[0].GetProperty("RecordCount");
+            //JsonElement recordCountElement = rootEnumerated[0].GetProperty("RecordCount");
             string jsonRecordCount = JsonSerializer.Serialize(new[]
             {
                 new
